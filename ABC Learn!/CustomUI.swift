0ec7c0c5abb309bb1a8f1f9004ae5c56fc3f3fbc
xyphergrim <johnclancy@outlook.com>
2016-11-23 13:30:13 -0600

    added some buttons

---
+++ ABC Learn!/CustomUI.swift
@@ -0,0 +1,31 @@
+//
+//  CustomUI.swift
+//  ABC Learn!
+//
+//  Created by John on 11/23/16.
+//  Copyright © 2016 jpclancy. All rights reserved.
+//
+
+import UIKit
+
+class CustomUI: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
